e9aa13ad46fe8f032740933d7678bb996c93f7f4
Lucas Nunes <dev.lucasnunes@gmail.com>
2024-07-08 19:17:13 -0300

    test: add token request test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  config.hosts << "6b98-191-9-38-105.ngrok-free.app"
   # Settings specified here will take precedence over those in config/application.rb.
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
