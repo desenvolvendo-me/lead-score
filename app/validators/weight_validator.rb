89f7c899f7b814684b84dc2a562810b3aea80860
mosca06 <67175987+mosca06@users.noreply.github.com>
2024-07-08 20:47:09 -0300

    Delete app/validators/weight_validator.rb

--- app/validators/weight_validator.rb
+++
@@ -1,9 +0,0 @@
-# app/validators/custom_validator.rb
-class WeightValidator < ActiveModel::Validator
-  def validate(record)
-    #TODO: Escrever aqui a regra de validação do json
-    # if record.question_answer.blank?
-    #   record.errors.add(:question_answer, "is invalid")
-    # end
-  end
-end
