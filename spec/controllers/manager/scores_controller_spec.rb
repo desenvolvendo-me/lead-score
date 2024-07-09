fac067bd4337255be327ce07780014f8535c24bf
alinecintracamargo <alineandradecintra@gmail.com>
2024-07-09 20:13:57 -0300

    feat: Add Scores tab to navigation bar

--- spec/controllers/manager/scores_controller_spec.rb
+++ spec/controllers/manager/scores_controller_spec.rb
@@ -1,16 +1,25 @@
+# spec/controllers/manager/scores_controller_spec.rb
+
 require 'rails_helper'
 
-RSpec.describe ScoresController, type: :controller do
-  describe "GET #index" do
-    it "returns a successful response" do
+RSpec.describe Manager::ScoresController, type: :controller do
+  describe 'GET #index' do
+    it 'assigns @scores' do
+      # Simulate user authentication (example with Devise)
+      user = create(:user)  # Assuming you have a User model and a factory setup
+      sign_in user
+
       get :index
-      expect(response).to be_successful
+      expect(assigns(:scores)).to eq(Score.all)
     end
 
-    it "renders the correct template" do
-      get :index
-      expect(response).to render_template('manager/scores/index')
-    end
+    it 'renders the index template' do
+      # Simulate user authentication
+      user = create(:user)
+      sign_in user
 
+      get :index
+      expect(response).to render_template('index')
+    end
   end
 end
