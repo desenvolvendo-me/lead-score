94f257c72bb2472f8cac9467959056c2ff55c982
alinecintracamargo <alineandradecintra@gmail.com>
2024-07-10 10:32:41 -0300

    chore: remove unnecessary comments

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,19 +1,10 @@
-# == Schema Information
-#
-# Table name: scores
-#
-#  id         :bigint           not null, primary key
-#  name       :string
-#  value      :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
+
 class Score < ApplicationRecord
-  # validations
+
   validates :name, presence: true
   validates :value, presence: true
 
-  # JSONB field
+
   attribute :data, :jsonb, default: {}
 
   def to_s
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,15 +1,36 @@
-# == Schema Information
-#
-# Table name: scores
-#
-#  id         :bigint           not null, primary key
-#  name       :string
-#  value      :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
+# spec/models/score_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe Score, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      score = Score.new(name: 'Test Score', value: 10)
+      expect(score).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      score = Score.new(name: nil, value: 10)
+      expect(score).to_not be_valid
+    end
+
+    it 'is not valid without a value' do
+      score = Score.new(name: 'Test Score', value: nil)
+      expect(score).to_not be_valid
+    end
+  end
+
+  describe 'default values' do
+    it 'has a default value for data' do
+      score = Score.new(name: 'Test Score', value: 10)
+      expect(score.data).to eq({})
+    end
+  end
+
+  describe '#to_s' do
+    it 'returns the name of the score' do
+      score = Score.new(name: 'Test Score', value: 10)
+      expect(score.to_s).to eq('Test Score')
+    end
+  end
 end
