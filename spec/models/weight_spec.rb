ef897ee1037722c7023f5d1a64d0a1cd7680462f
ReginaTR <regininhar@hotmail.com>
2024-07-11 11:25:57 -0300

    test(question): test validate json

--- spec/models/weight_spec.rb
+++ spec/models/weight_spec.rb
@@ -12,30 +12,34 @@
 require 'rails_helper'
 
 RSpec.describe WeightValidator do
-  describe "#valid_structure?" do
-    it "returns true for valid JSON structure" do
-      json_data = '{"pergunta1": {"resposta1": 10, "resposta2": 5}}'
-      expect(WeightValidator.valid_structure?(json_data)).to be_truthy
-    end
+  it "valid" do
+    weight = Weight.create(description: "Turma 21", status: "active", question_answer: { "pergunta1" => { "resposta1" => 10, "resposta2" => 5 }})
 
-    it "returns false if JSON is not a hash" do
-      json_data = '[{"pergunta1": {"resposta1": 10, "resposta2": 5}}]'
-      expect(WeightValidator.valid_structure?(json_data)).to be_falsey
-    end
-
-    it "returns false if keys are not strings" do
-      json_data = '{123: {"resposta1": 10, "resposta2": 5}}'
-      expect(WeightValidator.valid_structure?(json_data)).to be_falsey
-    end
-
-    it "returns false if inner keys are not strings" do
-      json_data = '{"pergunta1": {123: 10, "resposta2": 5}}'
-      expect(WeightValidator.valid_structure?(json_data)).to be_falsey
-    end
-
-    it "returns false if inner values are not numeric" do
-      json_data = '{"pergunta1": {"resposta1": "peso", "resposta2": 5}}'
-      expect(WeightValidator.valid_structure?(json_data)).to be_falsey
-    end
+    expect(weight.valid?).to be_truthy
   end
+
+  it "invalid" do
+    weight = Weight.create(description: "Turma 21", status: "active", question_answer: { "pergunta1" => { "resposta1" => 10, "resposta2" => "" } } )
+
+    expect(weight.valid?).to be_falsey
+  end
+
+  it "invalid" do
+    weight = Weight.create(description: "Turma 21", status: "active", question_answer: { "" => { "resposta1" => 10, "resposta2" => 5 } } )
+
+    expect(weight.valid?).to be_falsey
+  end
+
+  it "invalid" do
+    weight = Weight.create(description: "Turma 21", status: "active", question_answer: { "pergunta1" => { "" => 10, "resposta2" => 5 } } )
+
+    expect(weight.valid?).to be_falsey
+  end
+
+  it "invalid" do
+    weight = Weight.create(description: "Turma 21", status: "active", question_answer: { "pergunta1" => { "resposta1" => "", "resposta2" => 5 } } )
+
+    expect(weight.valid?).to be_falsey
+  end
+
 end
