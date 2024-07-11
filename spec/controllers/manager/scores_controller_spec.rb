508523863cef7e793725969ec5bdd35f84c12349
alinecintracamargo <alineandradecintra@gmail.com>
2024-07-11 10:20:07 -0300

    test: add model tests for Score

--- spec/controllers/manager/scores_controller_spec.rb
+++ spec/controllers/manager/scores_controller_spec.rb
@@ -1,20 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Manager::ScoresController, type: :controller do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+    get :index
+  end
+
   describe 'GET #index' do
     it 'assigns @scores' do
-      user = create(:user)
-      sign_in user
-
-      get :index
       expect(assigns(:scores)).to eq(Score.all)
     end
 
-    it 'renders the index template' do
-      user = create(:user)
-      sign_in user
+    it 'assigns @scores to not be nil' do
+      expect(assigns(:scores)).to_not be_nil
+    end
 
-      get :index
+    it 'renders the index template' do
       expect(response).to render_template('index')
     end
   end
