c0994d020aff2342ea57233ff619e1d92ebadb82
ReginaTR <regininhar@hotmail.com>
2024-07-11 11:44:05 -0300

    Revert "feat(question): model"
    This reverts commit f2f516e897bfcf2ea2bb21a6d7f3e0e6e0034cb8.

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -10,5 +10,13 @@
 #  updated_at      :datetime         not null
 #
 class Weight < ApplicationRecord
-  validates :question_answer, weight: true
+  validate :validate_question_answer_structure
+
+  private
+
+  def validate_question_answer_structure
+    unless WeightValidator.valid_structure?(question_answer.to_json)
+      errors.add(:question_answer, "has an invalid structure")
+    end
+  end
 end
