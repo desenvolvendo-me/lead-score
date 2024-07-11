bec4f92a4cd814284c9f520e2fa1ff1fdc150016
ReginaTR <regininhar@hotmail.com>
2024-07-11 11:45:43 -0300

    feat(question): model

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -10,13 +10,5 @@
 #  updated_at      :datetime         not null
 #
 class Weight < ApplicationRecord
-  validate :validate_question_answer_structure
-
-  private
-
-  def validate_question_answer_structure
-    unless WeightValidator.valid_structure?(question_answer.to_json)
-      errors.add(:question_answer, "has an invalid structure")
-    end
-  end
+  validates :question_answer, weight: true
 end
