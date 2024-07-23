1c685db3372d165c2cadb3db9990b71ad069bfe7
ReginaTR <regininhar@hotmail.com>
2024-07-23 15:22:05 -0300

    refactor: remove format json from weight controller

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -20,10 +20,8 @@ class WeightsController < ApplicationController
     respond_to do |format|
       if @weight.save
         format.html { redirect_to @weight, notice: 'Weight was successfully created.'}
-        format.json { render :show, status: :created, location: @weight }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @weight.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -32,20 +30,16 @@ class WeightsController < ApplicationController
     respond_to do |format|
       if @weight.update(weight_params)
         format.html { redirect_to @weight, notice: 'Weight was successfully updated.'}
-        format.json { render :show, status: :ok, location: @weight }
       else
         format.html { render :edit, status: :unprocessable_entity}
-        format.json { render json: @weight.errors, status: :unprocessable_entity}
       end
     end
   end
-
 
   def destroy
     @weight.destroy
     respond_to do |format|
       format.html { redirect_to weights_url, notice: 'Weight was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -58,5 +52,4 @@ class WeightsController < ApplicationController
   def weight_params
     params.require(:weight).permit(:description, :status, :question_answer)
   end
-
 end
