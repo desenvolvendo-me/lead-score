125b02ebab56f78a07f6303b5c18c737c33e7555
alinecintracamargo <alineandradecintra@gmail.com>
2024-07-23 13:46:40 -0300

    test: add tests for export functionality to the Scores controller

--- spec/controllers/manager/scores_controller_spec.rb
+++ spec/controllers/manager/scores_controller_spec.rb
@@ -7,21 +7,17 @@ RSpec.describe Manager::ScoresController, type: :controller do
   let!(:score2) { Score.create(name: 'Score 2', value: 20) }
 
   before do
-    sign_in user # Autentica o usuário
+    sign_in user
   end
 
   describe 'GET #export' do
     it 'exports scores to CSV and sends the file' do
-      # Simula a requisição GET para a ação export
       get :export, params: { q: { s: 'value asc' } }
 
-      # Verifique o tipo de conteúdo
       expect(response.content_type).to eq('text/html; charset=utf-8')
 
-      # Verifique o cabeçalho de disposição do conteúdo
       expect(response.headers['Content-Disposition']).to include('scores-')
 
-      # Verifique o conteúdo do CSV
       csv = CSV.parse(response.body, headers: true)
       expect(csv.headers).to include('Name', 'Score')
       expect(csv.find { |row| row['Name'] == 'Score 1' }['Score']).to eq('10')
