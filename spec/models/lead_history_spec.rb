1c9f9726114b2fc11cd53eace230c5d2febacac7
ReginaTR <regininhar@hotmail.com>
2024-08-05 08:32:15 -0300

    test: test model lead histories

--- spec/models/lead_history_spec.rb
+++ spec/models/lead_history_spec.rb
@@ -1,25 +1,22 @@
-# == Schema Information
-#
-# Table name: lead_histories
-#
-#  id          :bigint           not null, primary key
-#  destination :string
-#  lead        :string
-#  sent_at     :datetime
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  score_id    :bigint           not null
-#
-# Indexes
-#
-#  index_lead_histories_on_score_id  (score_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (score_id => scores.id)
-#
 require 'rails_helper'
 
 RSpec.describe LeadHistory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:score) { create(:score) }
+  let(:lead_history) { build(:lead_history, score: score) }
+
+  describe 'associations' do
+    it { should belong_to(:score) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:score) }
+    it { should validate_presence_of(:sent_at) }
+    it { should validate_presence_of(:destination) }
+  end
+
+  describe 'factory' do
+    it 'has a valid factory' do
+      expect(lead_history).to be_valid
+    end
+  end
 end
