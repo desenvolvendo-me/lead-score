5c24c09cbe275a369a111cec5d68c7fad0047fcd
Cassio Santana <cassioo.roger@gmail.com>
2024-08-16 16:15:06 -0300

    test: add SurveyParticipation model specs
    Ref #1 #6 #56

--- spec/models/survey_participation_spec.rb
+++ spec/models/survey_participation_spec.rb
@@ -10,5 +10,28 @@
 require 'rails_helper'
 
 RSpec.describe SurveyParticipation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:question_answer_pair) }
+  end
+
+  context 'when question_answer_pair is empty' do
+    let(:empty_question_answer_pair) do
+      described_class.new(question_answer_pair: {})
+    end
+
+    it 'is not valid' do
+      expect(empty_question_answer_pair).not_to be_valid
+      expect(empty_question_answer_pair.errors[:question_answer_pair])
+        .to include("can't be empty")
+    end
+  end
+
+  context 'when question_answer_pair is non-empty' do
+    let(:valid_question_answer_pair) do
+      described_class.new(question_answer_pair: { 'key' => 'value' })
+    end
+    it 'is valid' do
+      expect(valid_question_answer_pair).to be_valid
+    end
+  end
 end
