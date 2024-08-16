0a04355e137bbaaaa0df8b85497b918f90a22197
Cassio Santana <cassioo.roger@gmail.com>
2024-08-16 09:44:10 -0300

    test: add SurveyParticipation
    Ref #1 #6 #56

---
+++ spec/models/survey_participation_spec.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: survey_participations
+#
+#  id                   :bigint           not null, primary key
+#  question_answer_pair :jsonb
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe SurveyParticipation, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
