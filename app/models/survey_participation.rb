58374b4ffeead34241113d347afe00167de30749
Cassio Santana <cassioo.roger@gmail.com>
2024-08-26 23:19:37 -0300

    feat(SurveyParticipation): add consistency checker
    Ref #1 #6 #67

--- app/models/survey_participation.rb
+++ app/models/survey_participation.rb
@@ -13,41 +13,11 @@ class SurveyParticipation < ApplicationRecord
   validates :question_answer_pair, presence: true
   validates :question_answer_pair, non_empty_json: true
 
-  before_create :check_consistency
+  before_create -> { SurveyParticipations::ConsistencyChecker.call(self) }
 
   enum consistency_status: {
     consistent: 'consistent',
     inconsistent: 'inconsistent',
     pending: 'pending'
   }
-
-  private
-
-  def check_consistency
-    self.inconsistency_details = []
-
-    question_answer_pair.each do |question, answer|
-      if question.blank?
-        add_inconsistency('The question is blank')
-      elsif answer.blank?
-        add_inconsistency('The answer is blank')
-      else
-        weight = Weight.find_by("question_answer ? :key", key: question)
-
-        if weight.nil?
-          add_inconsistency('The question was not found')
-        elsif !weight.question_answer[question].key?(answer)
-          add_inconsistency('The answer was not found')
-        end
-      end
-    end
-
-    self.consistency_status = inconsistency_details.empty? ? :consistent : :inconsistent
-  end
-
-  def add_inconsistency(message)
-    return if inconsistency_details.include?(message)
-
-    inconsistency_details << message
-  end
 end
