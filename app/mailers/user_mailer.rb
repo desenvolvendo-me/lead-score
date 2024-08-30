735637034da83b9f6ed3b434fa070afd171c2cce
ReginaTR <regininhar@hotmail.com>
2024-08-30 16:01:49 -0300

    feat: create mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  def send_scores_csv
+    @user = params[:user]
+    file_path = params[:file_path]
+
+    attachments["scores-#{Date.today}.csv"] = File.read(file_path)
+    mail(to: @user.email, subject: 'Exportação de Scores')
+  end
+end
+
+
