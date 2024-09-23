dee56af842482a9cd89f216faec747b2db791e47
ReginaTR <regininhar@hotmail.com>
2024-09-23 18:37:24 -0300

    feat: export cvs and create a button to send email

--- app/controllers/manager/scores_controller.rb
+++ app/controllers/manager/scores_controller.rb
@@ -17,10 +17,22 @@ module Manager
       file_path = Rails.root.join('tmp', "scores-#{Date.today}.csv")
       File.write(file_path, csv_data)
 
-      flash[:notice] = "O CSV está sendo enviado por e-mail e baixado automaticamente."
-      ExportScoresWorker.perform_async(current_user.id, file_path.to_s)
+      ExportScoresJob.perform_async(current_user.id, file_path.to_s)
 
       send_data csv_data, filename: "scores-#{Date.today}.csv", type: 'text/csv', disposition: 'attachment'
+    end
+
+    def send_csv_email
+      scores = Score.all
+      csv_data = Scoring::ScoreCsvExporter.generate(scores)
+
+      file_path = Rails.root.join('tmp', "scores-#{Date.today}.csv")
+      File.write(file_path, csv_data)
+
+      ExportScoresJob.perform_async(current_user.id, file_path.to_s)
+
+      flash[:notice] = "Arquivo CSV enviado por e-mail com sucesso."
+      redirect_to manager_scores_path
     end
 
     private
--- app/workers/export_scores_job.rb
+++ app/workers/export_scores_job.rb
@@ -1,13 +1,9 @@
-class ExportScoresWorker
-  include Sidekiq::Worker
+class ExportScoresJob
+  include Sidekiq::Job
 
   def perform(user_id, file_path)
     user = User.find(user_id)
-
-    # Envia o e-mail com o CSV gerado
     UserMailer.with(user: user, file_path: file_path).send_scores_csv.deliver_now
-
-    # Remove o arquivo temporário após o envio
     File.delete(file_path) if File.exist?(file_path)
   end
 end
